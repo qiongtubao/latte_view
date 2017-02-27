f518577d1b873f5d1164ab639233dbc9d577fe7a
qiongtubao <810776886@qq.com>
2017-02-27 11:36:58 +0800

    add canvas context

--- src/canvas/context/index.js
+++ src/canvas/context/index.js
@@ -1,3 +1,30 @@
 (function() {
+	var Context = function(dom) {
+		if (!dom) {
+			dom = Document.create("canvas");
+			dom.width = 1000;
+			dom.height = 1000;
+		}
+		this.context = dom.getContext("2d");
+		this.width = dom.width;
+		this.height = dom.height;
+	};
+	(function() {
+		this.drawImage = function(lade, child, parent) {
 
-}).call(module.exports);
+		}
+		this.drawText = function(lade, child, parent) {
+
+		}
+		this.drawBox = function(lade, child, parent) {
+
+		} 
+		this.getImageArray = function() {
+
+		}
+	}).call(Context.prototype);
+	this.create = function(dom) {
+		var context = new Context(dom);
+		return context;
+	}
+}).call(module.exports);	
